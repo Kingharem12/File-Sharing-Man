500489520b31108e4c218ea9b834ae6e46e75bc7
Kingharem12 <32841183+Kingharem12@users.noreply.github.com>
2022-05-06 20:48:04 +0700

    Update config.py

--- config.py
+++ config.py
@@ -32,6 +32,8 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL 2 = int(os.environ.get("FORCE_SUB_CHANNEL",0"))
+FORCE_SUB_CHANNEL 3 = int(os.environ.get("FORCE_SUB_CHANNEL",0"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
